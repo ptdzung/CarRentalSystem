32261d6703b844c34b9cbbd5cb91da0ec2df30d3
Daniel Pham <56713013+ptdzung@users.noreply.github.com>
2019-11-15 01:15:14 +0800

    for RentalRateEntity

---
+++ CarRentalSystemLibrary/src/util/exception/RentalRateNotFoundException.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util.exception;
+
+/**
+ *
+ * @author Pham The Dzung
+ */
+public class RentalRateNotFoundException extends Exception {
+
+    public RentalRateNotFoundException() {
+    }
+
+    public RentalRateNotFoundException(String message) {
+        super(message);
+    }
+    
+    
+}
