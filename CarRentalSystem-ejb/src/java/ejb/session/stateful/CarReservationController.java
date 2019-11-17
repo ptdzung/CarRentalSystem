fa72fc05c99289ea7eda2cfadfe20fd13fbbd1b3
Daniel Pham <56713013+ptdzung@users.noreply.github.com>
2019-11-17 22:32:24 +0800

    for 1st submission

--- CarRentalSystem-ejb/src/java/ejb/session/stateful/CarReservationController.java
+++ CarRentalSystem-ejb/src/java/ejb/session/stateful/CarReservationController.java
@@ -5,11 +5,23 @@
  */
 package ejb.session.stateful;
 
+import ejb.session.stateless.CarSessionBeanLocal;
 import ejb.session.stateless.CustomerSessionBeanLocal;
+import ejb.session.stateless.RentalRateSessionBeanLocal;
 import ejb.session.stateless.ReservationRecordSessionBeanLocal;
+import entity.CarCategoryEntity;
+import entity.CarEntity;
+import entity.CarModelEntity;
+import entity.OutletEntity;
 import entity.OwnCustomerEntity;
 import entity.PartnerEntity;
+import entity.RentalDayEntity;
+import entity.RentalRateEntity;
 import entity.RentalRecordEntity;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Local;
@@ -20,6 +32,9 @@ import javax.persistence.PersistenceContext;
 import util.exception.InvalidLoginCredentialException;
 import util.exception.RentalRecordNotFoundException;
 import util.exception.EntityMismatchException;
+import util.exception.InputDataValidationException;
+import util.exception.RentalRateNotFoundException;
+import util.exception.UnknownPersistenceException;
 
 /**
  *
@@ -30,6 +45,12 @@ import util.exception.EntityMismatchException;
 @Remote(CarReservationControllerRemote.class)
 public class CarReservationController implements CarReservationControllerRemote, CarReservationControllerLocal {
 
+    @EJB
+    private RentalRateSessionBeanLocal rentalRateSessionBean;
+    
+    @EJB
+    private CarSessionBeanLocal carSessionBean;
+    
     @EJB
     private CustomerSessionBeanLocal customerSessionBean;
        
@@ -66,28 +87,120 @@ public class CarReservationController implements CarReservationControllerRemote,
     }
     
     @Override
-    public List<RentalRecordEntity> retrieveAllCustomerReservation() {
+    public List<RentalRecordEntity> retrieveAllReservation() {
         return reservationRecordSessionBean.retrieveRentalRecordsByCustomer(customer);
     }
     
     @Override
-    public String retrieveCustomerReservationDetails(Long resId) throws RentalRecordNotFoundException, EntityMismatchException {
-        return reservationRecordSessionBean.retrieveCustomerReservationDetails(resId, customer.getCustomerId());
+    public String retrieveReservationDetails(Long resId) throws RentalRecordNotFoundException, EntityMismatchException {
+        return reservationRecordSessionBean.retrieveReservationDetails(resId, customer.getCustomerId());
     }
     
     @Override
     public String retrievePartnerReservationDetails(Long resId) throws RentalRecordNotFoundException, EntityMismatchException {
-        return reservationRecordSessionBean.retrieveCustomerReservationDetails(resId, partner.getPartnerId());
+        return reservationRecordSessionBean.retrievePartnerReservationDetails(resId, partner.getPartnerId());
     }
     
     @Override
     public String cancelReservation(Long resId) throws RentalRecordNotFoundException {
         return reservationRecordSessionBean.cancelReservation(resId);
     }
+
+    @Override
+    public List<CarEntity> searchCar(CarCategoryEntity category, CarModelEntity model, Date startDate, Date endDate, OutletEntity pickupOutlet) throws RentalRateNotFoundException {
+        List<RentalRateEntity> availableRates = rentalRateSessionBean.retrieveRentalRatesForSearch(category, startDate, endDate);
+        if (availableRates.isEmpty()) {
+            throw new RentalRateNotFoundException("There is no available rental rates for this car category!");
+        }
+        
+        List<CarEntity> list = new ArrayList<>();
+        list = carSessionBean.searchCar(category, model, startDate, endDate, pickupOutlet);
+        return list;
+    }
     
     @Override
-    public List<RentalRecordEntity> retrieveAllPartnerReservation() {
-       return reservationRecordSessionBean.retrieveRentalRecordsByPartner(partner);
+    public List<RentalRateEntity> retrieveListOfOptimalRates(CarCategoryEntity category, Date startDate, Date endDate) {
+        List<RentalRateEntity> rates = rentalRateSessionBean.retrieveRentalRatesForSearch(category, startDate, endDate);
+        List<Date> dates = getListOfDates(startDate, endDate);
+        
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(startDate);
+        while(true) {
+            dates.add(calendar.getTime());
+            if (calendar.DATE == endDate.getDate() && calendar.MONTH == endDate.getMonth() && calendar.YEAR == endDate.getYear()) {
+                break;
+            }
+            calendar.setTime(new Date(calendar.getTimeInMillis() + 86400000));
+            calendar.set(calendar.HOUR, 0);
+            calendar.set(calendar.MINUTE, 0);
+        }
+        dates.add(endDate);
+        List<RentalRateEntity> returnRates = new ArrayList<>();
+        
+        for (Date d : dates) {
+            List<RentalRateEntity> availableRates = new ArrayList<>();
+            for (RentalRateEntity r: rates) {
+                if (r.getStartDate() == null && r.getEndDate() == null) {
+                    availableRates.add(r);
+                } else if (d.after(r.getStartDate()) || d.before(r.getEndDate())) {
+                    availableRates.add(r);
+                }
+            }
+            RentalRateEntity optimal = rentalRateSessionBean.getOptimalRate(availableRates);
+            returnRates.add(optimal);
+        }
+        
+        return returnRates;
     }
+
+    @Override
+    public Long createNewReservation(String creditCardNumber, Boolean hasPaid, CarCategoryEntity category, CarModelEntity model, Date startDate, Date endDate, OutletEntity pickupOutlet, OutletEntity returnOutlet, BigDecimal totalAmount) {
+        Long newRecordId = -1l;
+        
+        try {
+            newRecordId = reservationRecordSessionBean.createNewRentalRecord(new RentalRecordEntity(startDate, endDate, creditCardNumber, customer, 
+                                                                                  model, category, totalAmount, pickupOutlet, returnOutlet, hasPaid));
+            RentalRecordEntity curr = reservationRecordSessionBean.retrieveReservationById(newRecordId);
+            
+            List<Date> dates = getListOfDates(startDate, endDate);
+            List<RentalRateEntity> rates = retrieveListOfOptimalRates(category, startDate, endDate);
+            for (int i = 0; i < dates.size(); i++) {
+                RentalDayEntity day = new RentalDayEntity(dates.get(i), rates.get(i), curr);
+                em.persist(day);
+                em.flush();
+            }
+            
+        } catch (RentalRecordNotFoundException | InputDataValidationException | UnknownPersistenceException ex) {
+            System.out.println(ex.getMessage() + "\n");
+        }
+        
+        return newRecordId;
+    }
+    
+    private List<Date> getListOfDates(Date startDate, Date endDate) {
+        List<Date> dates = new ArrayList<>();
+        
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(startDate);
+        while(true) {
+            dates.add(calendar.getTime());
+            if (calendar.DATE == endDate.getDate() && calendar.MONTH == endDate.getMonth() && calendar.YEAR == endDate.getYear()) {
+                break;
+            }
+            calendar.setTime(new Date(calendar.getTimeInMillis() + 86400000));
+            calendar.set(calendar.HOUR, 0);
+            calendar.set(calendar.MINUTE, 0);
+        }
+        dates.add(endDate);
+        
+        return dates;
+    }
+
+    @Override
+    public List<RentalRecordEntity> retrieveAllPartnerReservation() {
+        return reservationRecordSessionBean.retrieveRentalRecordsByPartner(partner);
+    }
+    
+    
     
 }
--- CarRentalSystem-ejb/src/java/ejb/session/stateful/CarReservationControllerLocal.java
+++ CarRentalSystem-ejb/src/java/ejb/session/stateful/CarReservationControllerLocal.java
@@ -5,11 +5,19 @@
  */
 package ejb.session.stateful;
 
+import entity.CarCategoryEntity;
+import entity.CarEntity;
+import entity.CarModelEntity;
+import entity.OutletEntity;
 import entity.OwnCustomerEntity;
+import entity.RentalRateEntity;
 import entity.RentalRecordEntity;
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 import util.exception.EntityMismatchException;
 import util.exception.InvalidLoginCredentialException;
+import util.exception.RentalRateNotFoundException;
 import util.exception.RentalRecordNotFoundException;
 
 
@@ -25,14 +33,19 @@ public interface CarReservationControllerLocal {
 
     public OwnCustomerEntity customerLogin(String username, String password) throws InvalidLoginCredentialException;
 
-    public List<RentalRecordEntity> retrieveAllCustomerReservation();
+    public List<RentalRecordEntity> retrieveAllReservation();
 
-    public String retrieveCustomerReservationDetails(Long resId) throws RentalRecordNotFoundException, EntityMismatchException;
+    public String retrieveReservationDetails(Long resId) throws RentalRecordNotFoundException, EntityMismatchException;
 
     public String cancelReservation(Long resId) throws RentalRecordNotFoundException;
-    
-    public List<RentalRecordEntity> retrieveAllPartnerReservation();
 
-    public String retrievePartnerReservationDetails(Long resId) throws RentalRecordNotFoundException, EntityMismatchException;
+    List<CarEntity> searchCar(CarCategoryEntity category, CarModelEntity model, Date startDate, Date endDate, OutletEntity pickupOutlet) throws RentalRateNotFoundException ;
     
+    List<RentalRateEntity> retrieveListOfOptimalRates(CarCategoryEntity category, Date startDate, Date endDate);
+
+    Long createNewReservation(String creditCardNumber, Boolean hasPaid, CarCategoryEntity category, CarModelEntity model, Date startDate, Date endDate, OutletEntity pickupOutlet, OutletEntity returnOutlet, BigDecimal totalAmount);
+
+    List<RentalRecordEntity> retrieveAllPartnerReservation();
+    
+    public String retrievePartnerReservationDetails(Long resId) throws RentalRecordNotFoundException, EntityMismatchException;
 }
