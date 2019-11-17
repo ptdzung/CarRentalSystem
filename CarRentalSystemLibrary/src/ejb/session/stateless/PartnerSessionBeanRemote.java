e3933c90f413f60297577da1b53c499dccd0577b
Trishpal <54445356+Trishpal@users.noreply.github.com>
2019-11-17 22:06:48 +0800

    Added partner session bean remote

---
+++ CarRentalSystemLibrary/src/ejb/session/stateless/PartnerSessionBeanRemote.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateless;
+
+import entity.PartnerEntity;
+import util.exception.InvalidLoginCredentialException;
+
+
+/**
+ *
+ * @author Trishpal
+ */
+
+public interface PartnerSessionBeanRemote {
+    
+    public PartnerEntity partnerLogin(String username, String password) throws InvalidLoginCredentialException;
+    
+     public Long createNewPartner(PartnerEntity partner);
+}
