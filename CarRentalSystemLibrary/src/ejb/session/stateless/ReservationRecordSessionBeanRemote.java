3ae0317da5216c828e477238590f533f80385a0d
Trishpal <54445356+Trishpal@users.noreply.github.com>
2019-11-17 22:08:38 +0800

    Updated

--- CarRentalSystemLibrary/src/ejb/session/stateless/ReservationRecordSessionBeanRemote.java
+++ CarRentalSystemLibrary/src/ejb/session/stateless/ReservationRecordSessionBeanRemote.java
@@ -6,6 +6,7 @@
 package ejb.session.stateless;
 
 import entity.CustomerEntity;
+import entity.PartnerEntity;
 import entity.RentalRecordEntity;
 import util.exception.EntityMismatchException;
 import util.exception.RentalRecordNotFoundException;
@@ -17,7 +18,8 @@ import java.util.List;
  * @author Trishpal
  */
 public interface ReservationRecordSessionBeanRemote {
-    public String retrieveReservationDetails(Long resId, Long customerId) throws RentalRecordNotFoundException, EntityMismatchException;
+    
+    public String retrieveCustomerReservationDetails(Long resId, Long customerId) throws RentalRecordNotFoundException, EntityMismatchException;
 
     public String cancelReservation(long resId) throws RentalRecordNotFoundException;
     
@@ -26,6 +28,11 @@ public interface ReservationRecordSessionBeanRemote {
     public List<RentalRecordEntity> retrieveRentalRecordByDate(Date date);
 
     List<RentalRecordEntity> retrieveRentalRecordsByCustomer(CustomerEntity cus);
+    
+    public List<RentalRecordEntity> retrieveRentalRecordsByPartner(PartnerEntity part);
 
     boolean carAllocation(Date date);
+    
+    public String retrievePartnerReservationDetails(Long resId, Long partnerId) throws RentalRecordNotFoundException, EntityMismatchException;
+
 }
